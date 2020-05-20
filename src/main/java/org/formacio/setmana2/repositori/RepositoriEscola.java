2cd9eeac73362d2cfc81e48773bc9aba73380818
unknown <darques@cifpfbmoll.eu>
2020-05-20 18:31:28 +0200

    feat: Llegir alumne i curs de la bdd

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -1,23 +1,32 @@
 package org.formacio.setmana2.repositori;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.formacio.setmana2.domini.Alumne;
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Repository
 public class RepositoriEscola {
 
+	@PersistenceContext
+	EntityManager em;
 	
 	public Curs carregaCurs(String nom) {
-		return null;
+		return em.find(Curs.class, nom);
 	}
 	
-	
-	public Matricula apunta (String alumne, String curs) throws EdatIncorrecteException {
-	    return null;	
+	public Alumne carregaAlumne(String nom) {
+		return em.find(Alumne.class, nom);
 	}
 	
+
 	
 }
